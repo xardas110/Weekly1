3378aca4c318ebc69d2430e2c0732803efd30736
Ali Al-Awadi <ali.al-awadi@hotmail.com>
2020-09-05 11:18:42 +0200

    Finished

---
+++ Task5/Task5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//printf("\x1B[31mTexting\033[0m\t\t");
+	const char* star = "\x1B[31m * \033[0m";
+	printf("   1   2   3   4   5   6  \n");
+	printf("  --- --- --- --- --- --- \n");
+	printf("A:%s:%s:%s:%s:%s:%s : A\n", star, star, star, star, star, star);
+	printf("  --- --- --- --- --- --- \n");
+	printf("B:%s:%s:%s:%s:%s:%s : B\n", star, star, star, star, star, star);
+	printf("  --- --- --- --- --- --- \n");
+	printf("C:%s:%s:%s:%s:%s:%s : C\n", star, star, star, star, star, star);
+	printf("  --- --- --- --- --- --- \n");
+	printf("D:%s:%s:%s:%s:%s:%s : D\n", star, star, star, star, star, star);
+	printf("  --- --- --- --- --- --- \n");
+	printf("   1   2   3   4   5   6  \n");
+
+	system("pause");
+}
